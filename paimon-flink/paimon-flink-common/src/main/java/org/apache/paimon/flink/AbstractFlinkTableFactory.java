f0bf5c092411436254fb2d202bda6710e91a70fd
wgcn <1026688210@qq.com>
2023-07-12 11:21:23 +0800

    release 1

--- paimon-flink/paimon-flink-common/src/main/java/org/apache/paimon/flink/AbstractFlinkTableFactory.java
+++ paimon-flink/paimon-flink-common/src/main/java/org/apache/paimon/flink/AbstractFlinkTableFactory.java
@@ -34,6 +34,8 @@ import org.apache.paimon.flink.source.table.RichTableSource;
 import org.apache.paimon.lineage.LineageMeta;
 import org.apache.paimon.lineage.TableLineageEntity;
 import org.apache.paimon.lineage.TableLineageEntityImpl;
+import org.apache.paimon.fs.FileIOLoader;
+import org.apache.paimon.options.CatalogOptions;
 import org.apache.paimon.options.Options;
 import org.apache.paimon.schema.Schema;
 import org.apache.paimon.table.FileStoreTable;
@@ -202,8 +204,17 @@ public abstract class AbstractFlinkTableFactory
     }
 
     static CatalogContext createCatalogContext(DynamicTableFactory.Context context) {
-        return CatalogContext.create(
-                Options.fromMap(context.getCatalogTable().getOptions()), new FlinkFileIOLoader());
+        Options options = Options.fromMap(context.getCatalogTable().getOptions());
+        return createCatalogContextWithOption(options);
+    }
+
+    static CatalogContext createCatalogContextWithOption(Options options) {
+        String proxyUser = options.get(CatalogOptions.PROXY_USER);
+        FileIOLoader loader = null;
+        if (proxyUser == null) {
+            loader = new FlinkFileIOLoader();
+        }
+        return CatalogContext.create(options, loader);
     }
 
     static Table buildPaimonTable(DynamicTableFactory.Context context) {
